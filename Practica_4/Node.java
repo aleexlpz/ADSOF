f3bc9ddada4e05613fc1ac7a3cb447a3d249e934
sofiagarciah <sofyheras@gmail.com>
2024-03-21 15:28:04 +0100

    empezando p4

---
+++ Practica_4/Node.java
@@ -0,0 +1,50 @@
+package Practica_4;
+
+//Node: es el elemento básico para el funcionamiento del Blockchain. Cada nodo tiene un identificador único en la red, un Wallet
+//asociado y una lista de transacciones (con el objetivo de conocer cuales están confirmadas). Hay dos tipos de nodos: simples y
+//minadores. Los nodos simples están enfocados únicamente a hacer transacciones, mientras que los nodos minadores, además de
+//proporcionar la misma funcionalidad que los nodos simples, son capaces de minar y validar bloques (lo veremos en apartados
+//posteriores), por lo que definen una capacidad computacional medida en MIPS.
+public class Node extends Element{
+    private int id;
+    private static int nextId = 1;
+
+    private Wallet wallet;
+    private boolean isMiner;
+    private int mips;
+    private List<Transaction> transactions;
+
+
+
+    public Node(Wallet wallet) {
+        this.id = nextId++;
+        this.wallet = wallet;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Wallet getWallet() {
+        return wallet;
+    }
+
+    public boolean isMiner() {
+        return isMiner;
+    }
+
+    public int getMips() {
+        return mips;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+
+    @Override
+    public String toString() {
+        return this.wallet + " | @Node#" + this.getId();
+    }
+}
---
+++ Practica_4/Transaction.java
@@ -0,0 +1,43 @@
+package Practica_4;
+//Transaction: los Wallets pueden llevar a cabo transacciones entre ellos. Cada transacción tiene un identificador único e incluye la
+//clave del emisor, la clave del receptor y el valor de la transacción.
+public class Transaction {
+    private int id;
+    private static int nextId = 1;
+    private String senderKey;
+    private String receiverKey;
+    private int value;
+
+    public Transaction(String senderKey, String receiverKey, int value) {
+        this.id = nextId++;
+        this.senderKey = senderKey;
+        this.receiverKey = receiverKey;
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getSenderKey() {
+        return senderKey;
+    }
+
+    public String getReceiverKey() {
+        return receiverKey;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "id=" + id +
+                ", senderKey='" + senderKey + '\'' +
+                ", receiverKey='" + receiverKey + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
