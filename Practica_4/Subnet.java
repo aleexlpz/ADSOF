1fe58009851fa9136a045102ffb65b5a0beab86a
Sofia Garcia <98950631+sofiagarciah@users.noreply.github.com>
2024-04-08 15:32:39 +0200

    Update Subnet.java

--- Practica_4/Subnet.java
+++ Practica_4/Subnet.java
@@ -1,48 +1,39 @@
 package Practica_4;
 import java.util.*;
 
-import exceptions.*;
+public class Subnet implements IConnectable{
+    private int id;
+    private static int nextId = 1;
+    private List<Node>  nodes;
 
-/*Para soportar subredes (Subnet) dentro de redes (BlockchainNetwork), el método getParent devolverá
-el objeto IConnectable padre si lo hay, o null en caso contrario. El método default getTopParent devuelve el objeto IConnectable de
-nivel superior, y puede serte útil en apartados posteriores.*/
-public class Subnet extends BlockchainNetwork{
-    private List<Node> nodes;
-    private IConnectable parent;
+    public Subnet(Node... nodes) {
+        this.id = nextId++;
+        this.nodes = Arrays.asList(nodes);
 
-    public Subnet(String name) {
-        super(name);
-        this.nodes = new ArrayList<>();
-        this.parent = null;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public List<Node> getNodes() {
         return nodes;
     }
-    public IConnectable getParent(){
-        return this.parent;
-    }
-    public IConnectable getTopParent(){
-        if(this.parent == null){
-            return this;
-        }
-        return this.parent.getTopParent();
+
+    @Override
+    public IConnectable getParent() {
+        return null;
     }
 
-
-    public Subnet connect(Node node) throws ConnectionException {
-        if(this.nodes.contains(node)){
-            throw new ConnectionException(node.fullName() + "is already connected to the network");
+    @Override
+    public void broadcast(IMessage msg) {
+        System.out.println("[Subnet#"+this.getId()+"] "+msg.getMessage());
+        System.out.println("Broadcasting to "+this.nodes.size()+" nodes:");
+        for (Node nodo: this.nodes) {
+            nodo.broadcast(msg);
         }
-        for (Subnet s : this.getSubnets()){
-            if(s.getNodes().contains(node)){
-                throw new ConnectionException(node.fullName() + "is connected to a different network");
-            }
-        }
-        System.out.println("ADSOF blockchain - new peer connected: " + node);
-        this.nodes.add(node);
-        return this;
     }
+
 
 
     @Override
